c422abf4b0ce3e736eae20481d07e925938a2160
aliozer0 <aizer07@icloud.com>
2023-07-21 16:54:41 +0300

    test 5

--- lib/resnewscreen.dart
+++ lib/resnewscreen.dart
@@ -16,7 +16,7 @@ class ResNewScreen extends StatefulWidget {
 //color:Color(0xffD6E8DB);
 //color:Color(0xffA4907C);
 //color:Color(0xff);
-// test
+// tes
 
 class _ResNewScreenState extends State<ResNewScreen> {
   void _openNewScreen(BuildContext context) {
